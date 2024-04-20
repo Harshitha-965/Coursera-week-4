dfcb054038cf4add69d1092e071f1c4386502d32
Harshitha-965 <harshithakg09@gmail.com>
2024-04-20 18:45:20 +0530

    updated

--- mod4-sols/speakHello.js
+++ mod4-sols/speakHello.js
@@ -1,1 +1,9 @@
-
+(function(window){
+	var helloSpeaker={};
+	var speakWord = "Hello";
+	helloSpeaker.speak=function(name) {
+		helloSpeaker.name=name;
+		console.log(speakWord + " " + helloSpeaker.name);
+	}
+	window.helloSpeaker=helloSpeaker;
+})(window)
